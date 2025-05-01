64322d5b9e097c0314ceca959b16c9f116ada831
Torikul <devtorikul@gmail.com>
2025-05-01 19:54:26 +0600

    fix mobile side menubar

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const MobileNav = ({sidebar,setSidebar}) => {
   return (
-    <div className={`lg:hidden fixed w-[50%] h-[100vh] bg-white pt-20 z-9 right-0 ${sidebar ? "":"translate-x-full"} transition-all duration-300 border border-[#7070703e]`}>
+    <div className={`lg:hidden fixed w-[50%] md:w-[300px] h-[100vh] bg-white pt-2 sm:pt-4 px-2 sm:px-3 z-9 right-0 ${sidebar ? "":"translate-x-full"} transition-all duration-300 border border-[#7070703e]`}>
       <div className='flex flex-col p-5 gap-5'>
         <Link onClick={()=>{setSidebar(!sidebar)}} to={"/"}>ホーム</Link>
         <Link onClick={()=>{setSidebar(!sidebar)}} to={"/treatment"}>施術メニュー</Link>
